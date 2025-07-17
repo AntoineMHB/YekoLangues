1d795068125e2a9353c991f897065d4112ea83f2
AntoineMHB <antoinemuhumbania@gmail.com>
2025-07-18 01:05:15 +0200

    Add the calendly link on the commencer votre apprentissage button

---
+++ src/components/calendly.tsx
@@ -0,0 +1,11 @@
+export default function Calendly() {
+    return (
+        <>
+          {/* Calendly inline widget begin  */}
+           <div data-url="https://calendly.com/contact-yekolangues/cours-d-essai-gratuit" className="min-width:320px;height:700px;"></div>
+             <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+           {/* Calendly inline widget end  */}
+        </>
+    )
+
+}
