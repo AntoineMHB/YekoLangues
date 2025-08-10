865edc2508617563fbd7aa36aeea220d3462d14a
AntoineMHB <antoinemuhumbania@gmail.com>
2025-08-10 11:22:38 +0200

    Add two cards to call for learning

--- src/components/CourseOffering.tsx
+++ src/components/CourseOffering.tsx
@@ -4,6 +4,7 @@ import { courses } from "../data/courses";
 import CourseCard from "../components/CourseCard";
 import ComingSoon from "./ComingSoon";
 import manLearning from "../assets/manLearning.jpg";
+import blackWomanLearning from "../assets/blackWomanLearning.jpg";
 
 const CourseOfferings: React.FC = () => {
   const [activeTab, setActiveTab] = useState<"lingala" | "swahili">("lingala");
@@ -49,8 +50,8 @@ const CourseOfferings: React.FC = () => {
           </div>
         </div>
 
-        <div className="flex items-center justify-center mb-8">
-          <div className="relative z-[100] overflow-hidden  rounded-none transition-transform duration-500 hover:scale-110">
+        <div className="flex items-center justify-center mb-8 gap-5">
+          <div className="relative z-[100] overflow-hidden rounded-none transition-transform duration-500 hover:scale-105 hover:-rotate-x-6 perspective-[1000px]">
             <a
               href="/langues/lingala"
               className="absolute z-[100] top-[1px] left-[1px] h-[calc(100%-2px)] w-[calc(100%-2px)] block"
@@ -66,39 +67,43 @@ const CourseOfferings: React.FC = () => {
                 }}
               ></div>
 
-              <div className="relative z-10 text-center p-6 ">
+              <div className="relative z-10 text-center p-6 pt-32">
                 <h2 className="text-3xl font-bold text-white">
                   Apprendre le Lingala
                 </h2>
-                <h5 className="mt-2 text-lg text-white/80 max-w-2xl mx-auto">
-                  Parlée au cœur de l’Afrique centrale, le lingala est une
-                  langue vivante et riche en culture. Apprenez à la maîtriser
-                  grâce à nos cours conçus pour tous les niveaux.
+                <h5 className="mt-2 text-lg text-white/80 max-w-md mx-auto text-justify pt-20">
+                  Parlé au cœur de l’Afrique centrale, le lingala est une langue
+                  vivante et riche en culture. Apprenez à la maîtriser grâce à
+                  nos cours conçus pour tous les niveaux.
                 </h5>
               </div>
             </div>
           </div>
 
-          <div className="relative z-[100] bg-black/60 hover:bg-black/20 transition-colors duration-300 rounded-none">
+          <div className="relative z-[100] overflow-hidden rounded-none transition-transform duration-500 hover:scale-105 hover:-rotate-x-6 perspective-[1000px]">
             <a
-              href="/langues/swahili"
+              href="/langues/lingala"
               className="absolute z-[100] top-[1px] left-[1px] h-[calc(100%-2px)] w-[calc(100%-2px)] block"
             ></a>
 
             <div className="relative min-h-[500px]">
               <div
-                className="absolute inset-0 bg-cover bg-center
-                  background-image: url('https://somalangues.com/wp-content/uploads/2024/09/AdobeStock_432905701-min.webp');"
+                className="absolute inset-0 bg-cover bg-center"
+                style={{
+                  backgroundImage: `linear-gradient(to bottom,rgba(0,0,0,.6),rgba(0,0,0,.6)),url(${blackWomanLearning})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center 30%",
+                }}
               ></div>
 
-              <div className="relative z-10 p-6">
+              <div className="relative z-10 text-center p-6 pt-32">
                 <h2 className="text-3xl font-bold text-white">
                   Apprendre le Swahili
                 </h2>
-                <h5 className="mt-2 text-lg text-white/80">
-                  Le Swahili est l’une des langues les plus parlées en Afrique
-                  de l’Est. Développez vos compétences linguistiques avec des
-                  cours adaptés à tous les niveaux.
+                <h5 className="mt-2 text-lg text-white/80 max-w-md text-justify mx-auto pt-20">
+                  Découvrez le swahili, l’une des langues les plus parlées
+                  d’Afrique de l’Est et la plus parlée en Afrique. Progressez à
+                  votre rythme grâce à nos cours adaptés à tous les niveaux.
                 </h5>
               </div>
             </div>
